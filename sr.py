e6791e41ac986caa227c0190c15105195eaf30bd
Squid <squid@Squids-iMac.local>
2018-12-09 09:48:48 -0500

    Comments

--- sr.py
+++ sr.py
@@ -33,6 +33,13 @@ CHANNEL_COLORS = [
 	(0, 255, 50)
 ]
 
+FREQUENCY_LIMITS = [
+	(0, 1000),
+	(1000, 3000),
+	(3000, 5000),
+	(5000, 20000)
+]
+
 serial_port = serial.Serial("/dev/cu.usbmodem1441")
 
 def audio_in():
@@ -45,8 +52,6 @@ def audio_in():
 					frames_per_buffer=CHUNK,
 					input_device_index=DEVICE)
 
-	frequency_limits = [(0, 1000), (1000, 3000), (3000, 5000), (5000, 20000)]
-
 	samples = [CircularList(250) for i in range(CHANNELS)]
 
 	while True:
@@ -54,7 +59,7 @@ def audio_in():
 
 		if data != "":
 			try:
-				matrix = calculate_levels(data, frequency_limits)
+				matrix = calculate_levels(data)
 
 				if not np.isfinite(np.sum(matrix)):
 					continue
@@ -116,31 +121,37 @@ def send_list(leds):
 	serial_port.flush()
 	serial_port.flushInput()
 
-def calculate_levels(data, frequency_limits):
+def calculate_levels(data):
+	# Input is stereo, but we only want the left channel
+	# Note: data has 2 bytes per channel
 	data_stereo = np.frombuffer(data, dtype=np.int16)
-	data = np.empty(len(data) / CHANNELS)
+	data = np.empty(len(data) / 4)
 	data[:] = data_stereo[::2]
 
+	# Apply hanning window
 	data = data * np.hanning(len(data))
+
+	# FFT
 	fourier = np.fft.rfft(data)
+
+	# Remove one sample so array length is consistent
 	fourier = np.delete(fourier, len(fourier) - 1)
+
+	# Power spectrum
 	power = np.abs(fourier) ** 2
 
-	matrix = np.zeros(CHANNELS, dtype='float64')
+	matrix = np.zeros(CHANNELS, dtype=np.float64)
 
-	for pin in range(CHANNELS):
-		idx1 = int(CHUNK * frequency_limits[pin][0] / RATE)
-		idx2 = int(CHUNK * frequency_limits[pin][1] / RATE)
-
-		if idx1 == idx2:
-			idx2 += 1
-
+	for i in range(CHANNELS):
+		# Sum of powers over a range of frequencies
+		idx1 = int(CHUNK * FREQUENCY_LIMITS[i][0] / RATE)
+		idx2 = int(CHUNK * FREQUENCY_LIMITS[i][1] / RATE)
 		npsums = np.sum(power[idx1:idx2:1])
 
 		if npsums == 0:
-			matrix[pin] = 0
+			matrix[i] = 0
 		else:
-			matrix[pin] = np.log10(npsums)
+			matrix[i] = np.log10(npsums)
 
 	return matrix
 
